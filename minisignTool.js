96e98f1f8e7658cc154339384bde2136a5f85e27
Christophe Diederichs <chris.d.itunes@gmail.com>
2019-04-30 10:33:04 +0200

    minisignTool.js added

---
+++ minisignTool.js
@@ -0,0 +1,149 @@
+const fs = require('fs')
+const assert = require('assert')
+const sodium = require('sodium-native')
+const xor = require('buffer-xor')
+const reverse = require('buffer-reverse')
+
+// put path for file to be signed and signature file here
+const file = 'test.txt'
+const signatureFile = `${file}.minisig`
+const pubKeyFile = 'minisign.pub'
+const secKeyFile = 'minisign.key'
+const comment = '<insert comment here>'
+
+const untrustedPrelude = Buffer.from('untrusted comment: ')
+const trustedPrelude = Buffer.from('trusted comment: ')
+const untrustedCommentStart = untrustedPrelude.byteLength
+const expectedSignatureAlgorithm = Buffer.from('Ed')
+
+// password for kdf algorithm - must correspond to the minisign public key
+const passwordBuf = Buffer.from('')
+
+function parsePubkey (pubkeyBuf) {
+  assert(untrustedPrelude.equals(pubkeyBuf.subarray(0, untrustedCommentStart)))
+  const untrustedComment = pubkeyBuf.subarray(untrustedCommentStart, pubkeyBuf.indexOf('\n', untrustedCommentStart))
+
+  const keyInfoStart = untrustedCommentStart + untrustedComment.byteLength + 1
+  const keyInfoBase64 = pubkeyBuf.subarray(keyInfoStart, pubkeyBuf.indexOf('\n', keyInfoStart)).toString()
+  const keyInfo = Buffer.from(keyInfoBase64, 'base64')
+
+  const signatureAlgorithm = keyInfo.subarray(0, 2)
+  const keyId = reverse(keyInfo.subarray(2, 10)).toString('hex')
+  const publicKey = keyInfo.subarray(10)
+
+  assert(signatureAlgorithm.equals(expectedSignatureAlgorithm))
+
+  return {
+    untrustedComment,
+    signatureAlgorithm,
+    keyId,
+    publicKey
+  }
+}
+
+function parseSignature (signatureBuf) {
+  assert(untrustedPrelude.equals(signatureBuf.subarray(0, untrustedCommentStart)))
+
+  const untrustedCommentEnd = signatureBuf.indexOf('\n', untrustedCommentStart)
+  const untrustedComment = signatureBuf.subarray(untrustedCommentStart, untrustedCommentEnd)
+
+  const sigInfoStart = untrustedCommentEnd + 1
+  const sigInfoEnd = signatureBuf.indexOf('\n', sigInfoStart)
+  const sigInfoBase64 = signatureBuf.subarray(sigInfoStart, sigInfoEnd).toString()
+  const sigInfo = Buffer.from(sigInfoBase64, 'base64')
+
+  const signatureAlgorithm = sigInfo.subarray(0, 2)
+  const keyId = reverse(sigInfo.subarray(2, 10))
+  const signature = sigInfo.subarray(10, sigInfoEnd)
+
+  const trustedCommentStart = sigInfoEnd + 1 + trustedPrelude.byteLength
+  const trustedCommentEnd = signatureBuf.indexOf('\n', trustedCommentStart)
+  const trustedComment = signatureBuf.subarray(trustedCommentStart, trustedCommentEnd)
+
+  const globalSignatureBase64 = signatureBuf.subarray(trustedCommentEnd + 1)
+  const globalSignature = Buffer.from(globalSignatureBase64, 'base64')
+
+  return {
+    untrustedComment,
+    signatureAlgorithm,
+    keyId,
+    signature,
+    trustedComment,
+    globalSignature
+  }
+}
+
+function parseSecretKey (secretKeyBuf) {
+  assert(untrustedPrelude.equals(secretKeyBuf.subarray(0, untrustedCommentStart)))
+
+  const untrustedCommentEnd = secretKeyBuf.indexOf('\n', untrustedCommentStart)
+  const untrustedComment = secretKeyBuf.subarray(untrustedCommentStart, untrustedCommentEnd).toString('ascii')
+
+  const secretKeyInfoStart = untrustedCommentEnd + 1
+  const secretKeyInfoEnd = secretKeyBuf.indexOf('\n', secretKeyInfoStart)
+  const secretKeyInfoBase64 = secretKeyBuf.subarray(secretKeyInfoStart, secretKeyInfoEnd).toString()
+  const secretKeyInfo = Buffer.from(secretKeyInfoBase64, 'base64')
+
+  const signatureAlgorithm = secretKeyInfo.subarray(0, 2).toString()
+  const kdfAlgorithm = secretKeyInfo.subarray(2, 4).toString()
+  const cksumAlgorithm = secretKeyInfo.subarray(4, 6).toString()
+  const kdfSalt = secretKeyInfo.subarray(6, 38)
+  const kdfOpsLimit = sodium.crypto_pwhash_scryptsalsa208sha256_OPSLIMIT_SENSITIVE
+  const kdfMemLimit = sodium.crypto_pwhash_scryptsalsa208sha256_MEMLIMIT_SENSITIVE
+  const keynumSK = secretKeyInfo.subarray(secretKeyInfo.length - 104)
+
+  return {
+    untrustedComment,
+    signatureAlgorithm,
+    kdfAlgorithm,
+    cksumAlgorithm,
+    kdfSalt,
+    kdfOpsLimit,
+    kdfMemLimit,
+    keynumSK
+  }
+}
+
+function extractSecretKey (passwordBuf, kdfSalt, kdfOpsLimit, kdfMemLimit, keynumSK) {
+  var kdfOutput = Buffer.alloc(104)
+  var keynumInfo = Buffer.alloc(104)
+
+  sodium.crypto_pwhash_scryptsalsa208sha256(kdfOutput, passwordBuf, kdfSalt, kdfOpsLimit, kdfMemLimit)
+  keynumInfo = xor(kdfOutput, keynumSK)
+  const keyID = keynumInfo.subarray(0, 8)
+  const secretKey = keynumInfo.subarray(8, 72)
+  const checksum = keynumInfo.subarray(72)
+
+  return {
+    keyID,
+    secretKey,
+    checksum
+  }
+}
+
+// load secret key file
+fs.readFile(secKeyFile, function (err, SKbuf) {
+  if (err) throw err
+  var SKinfo = parseSecretKey(SKbuf)
+  SKinfo = extractSecretKey(passwordBuf, SKinfo.kdfSalt, SKinfo.kdfOpsLimit, SKinfo.kdfMemLimit, SKinfo.keynumSK)
+
+  var signatureTest = Buffer.alloc(sodium.crypto_sign_BYTES)
+  var globalSignatureTest = Buffer.alloc(sodium.crypto_sign_BYTES)
+  console.log(SKinfo.keyID)
+
+  // load content to be signed
+  fs.readFile(file, function (err, message) {
+    if (err) throw err
+    sodium.crypto_sign_detached(signatureTest, message, SKinfo.secretKey)
+    var forGlobalSig = Buffer.concat([signatureTest, comment])
+    sodium.crypto_sign_detached(globalSignatureTest, forGlobalSig, SKinfo.secretKey)
+    var toFile = ('untrusted comment: \n' + Buffer.concat([expectedSignatureAlgorithm, SKinfo.keyID, signatureTest]).toString('base64') + '\n' + 'trusted comment: ' + comment.toString('ascii') + '\n' + globalSignatureTest.toString('base64'))
+    console.log(toFile)
+
+    // write signature to file
+    fs.writeFile(signatureFile, toFile, function (err) {
+      if (err) throw err
+      console.log(`minsign signature saved to ${signatureFile}`)
+    })
+  })
+})
---
+++ node_modules/sodium-native/CHANGELOG.md
@@ -0,0 +1,116 @@
+# CHANGELOG
+
+## Current
+
+## v2.4.2
+
+* Do travis release on `lts/*` node version
+
+## v2.4.1
+
+* We cannot yet support Node 12 or Electron 5, so explicit versions on prebuildify.
+
+## v2.4.0
+
+* Fix documentation error (thanks @jedisct1)
+* Add `crypto_pwhash_scryptsalsa208sha256_*` functions and constants.
+
+## v2.3.0
+
+* Upgrade to libsodium 1.0.17
+* Add new `sodium_sub` (opposite of `sodium_add`)
+* Add new finite field operations `crypto_core_ed25519_*` and constants
+* Add `crypto_sign_ed25519_sk_to_pk`
+
+## v2.2.6
+
+* Rebuilding the electron prebuild to get 4.0.4 support to work. This has a fix
+  for an ABI mismatch.
+
+## v2.2.5
+
+* Rebuilding the electron prebuild to get 4.0.0 support to work.
+
+## v2.2.4
+
+* Updated cross-references to libsodium documentation (thanks @stripedpajamas)
+* Fix documentation typo (thanks @ralphtheninja)
+* Fix [DEP0005] DeprecationWarning: `Buffer()` (thanks @ralphtheninja)
+* Upgrade and be compliant with standard@12
+* Improve robustness of Windows builds. This means that we now use the "best"
+  possible MSBuild. Thank you for all the work @enko
+* Due to the previous effort we can now build all artefacts on Travis and their
+  new Windows offering
+* Amend the new MSBuild finding algorithm to look for "Program Files (x86)"
+  first, such that cross-compiling works
+
+## v2.2.3
+
+* Add Node 11 to build matrix
+
+## v2.2.2
+
+* Document release process
+* Wrong error messages wrt. `crypto_sign`. Thanks @jackschmidt
+* Build for Electron v3.0.0
+
+## v2.2.1
+
+* Fix CHANGELOG
+
+## v2.2.0
+
+* Register tags for `async_hook`s on `crypto_pwhash_*_async` functions
+* Add constants and methods for `crypto_aead_xchacha20poly1305_ietf_*`. Please
+  note the special circumstances around the bindings of `MESSAGEBYTES_MAX` and
+  `crypto_aead_xchacha20poly1305_ietf_*_detached`.
+* Improved error messages; now reports the constants and argument names
+  documented from javascript.
+* Use `Buffer.alloc`/`Buffer.fill`/`Buffer.from` in tests and examples
+* Add more libsodium helpers; `sodium_memcmp`, `sodium_compare`, `sodium_add`,
+  `sodium_increment`, `sodium_is_zero`
+* Make it possible to pass only one of `rx` or `tx` to `crypto_kx_*`
+* Add `crypto_scalarmult_ed25519_*` and `crypto_core_ed25519_*` operations
+
+## v2.1.6
+
+* Additional check `x < 0` before cast on uint assert macro
+* Add prebuilds for Node 10
+
+## v2.1.5
+
+Fixes a critical bug in `crypto_secretstream_xchacha20poly1305_init_push` where
+it would call `crypto_secretstream_xchacha20poly1305_init_pull` instead.
+
+## v2.1.4
+
+Only use the constants that `libsodium` compiled with instead of the ones that
+`sodium-native` compiled with. This has caused bugs for some users and may have
+led to subtle bugs.
+
+## v2.1.3
+
+Rework build process so it is more versatile on UNIX operating systems by
+parsing the libtool archive files for correct .so name. This fixes builds on
+OpenBSD (#54)
+
+## v2.1.2
+
+Fix `armv7l` builds.
+
+## v2.1.1
+
+A mistake was made in generating prebuilds for v2.1.0, this version resolves the
+issue.
+
+## v2.1.0
+- Upgrade to libsodium 1.0.16
+- Expose the new `crypto_secretstream` API
+- Expose `crypto_kx` API
+- Expose `sodium_pad` and `sodium_unpad` APIs
+- Expose `crypto_pwhash_str_needs_rehash`
+- Expose `randombytes_SEEDBYTES` `randombytes_random`, `randombytes_uniform` and
+  `randombytes_buf_deterministic`
+- Check for `NULL` on `sodium_malloc`
+- All "Secure Buffers" (created with `sodium_malloc`) now have an immutable
+  `.secure = true` property
